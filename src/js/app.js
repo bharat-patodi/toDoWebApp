d7aea7995ba075ac4ecd4bdc168c2784bdaa5dc7
bharatpatodi <bharatpatodi@gmail.com>
2020-01-09 11:42:53 +0530

    Fixed the removal issue. Now the state persistence is complete with both add and remove actions retaining their states.

--- src/js/app.js
+++ src/js/app.js
@@ -4,9 +4,11 @@ The main code
 -------------------------------------------------
 */
 window.onload = init;
-const allEntries = document.getElementById('entries');
+const choreContainer = document.getElementById('entries');
+const allEntries = document.getElementsByClassName('singleChore');
 const entryList = document.getElementById('entries').getElementsByTagName('input');
 const checkedAttribs = [];
+const classHidden = [];
 
 /*
 -------------------------------------------------
@@ -15,7 +17,7 @@ Initialization
 */
 
 function init() {
-    allEntries.innerHTML = localStorage.getItem('previousState');
+    choreContainer.innerHTML = localStorage.getItem('previousState');
     restoreChkbxStatus(entryList.length, 0);
     showRemoveBtn();
 }
@@ -30,7 +32,7 @@ function addEntry(e) {
     e.preventDefault();
 
     // Getting the value of the new entry
-    const previousEntries = allEntries.innerHTML;
+    const previousEntries = choreContainer.innerHTML;
     const newEntry = document.getElementById('chore').value;
 
     // Check if chore is empty
@@ -38,10 +40,9 @@ function addEntry(e) {
 
         checkState();
         // If chore is not empty, append the new entry above the old ones
-        const newEntryTemplate = "<li class = \"allEntries\"><input type=\"checkbox\"><span>" + newEntry + "</span></input><span class = \"delete-single-chore\" onclick = \" this.parentNode.style.display = 'none' \">&times;</span></li>";
+        const newEntryTemplate = "<li class = \"singleChore\"><input type=\"checkbox\"><span>" + newEntry + "</span></input><span class = \"delete-single-chore\" onclick = \"removeOne(event)\" >&times;</span></li>";
 
-        allEntries.innerHTML = newEntryTemplate + previousEntries;
-
+        choreContainer.innerHTML = newEntryTemplate + previousEntries;
         restoreChkbxStatus(entryList.length - 1, 1);
         checkState();
 
@@ -49,7 +50,10 @@ function addEntry(e) {
         document.getElementById('chore').value = "";
 
         // Data persistence -- Local storage
-        localStorage.setItem('previousState', allEntries.innerHTML);
+        localStorage.setItem('previousState', choreContainer.innerHTML);
+        // localStorage.setItem('previousStyles', window.getComputedStyle(choreContainer));
+        // console.log(window.getComputedStyle(choreContainer));
+        // console.log(allEntries);
 
         showRemoveBtn();
     }
@@ -58,7 +62,6 @@ function addEntry(e) {
         alert("You have not entered anything.");
     }
 }
-
 
 /*
 -------------------------------------------------
@@ -70,8 +73,11 @@ function checkState() {
         checkedAttribs[j] = entryList[j].checked;
         // Verify the checked values with a console statement
         // console.log(entryList[j].checked + "" + j);
+        classHidden[j] = entryList[j].parentNode.style.display;
     }
+    console.log(classHidden);
     localStorage.setItem('checkboxStatuses', JSON.stringify(checkedAttribs));
+    localStorage.setItem('displayStatuses', JSON.stringify(classHidden));
 }
 
 /*
@@ -82,8 +88,11 @@ Restoring the checkbox statuses
 
 function restoreChkbxStatus(len, increment) {
     const testArray = JSON.parse(localStorage.getItem('checkboxStatuses'));
+    const anotherArray = JSON.parse(localStorage.getItem('displayStatuses'));
     for (var k = 0; k < len; k++) {
         entryList[k + increment].checked = testArray[k];
+        entryList[k + increment].parentNode.style.display = anotherArray[k];
+
     }
 }
 
@@ -94,9 +103,11 @@ Removing a single entry - use e.target Also read up event bubbling for making th
 -------------------------------------------------
 */
 
-function removeOne(eve) {
-    console.log(eve.target);
-    console.log(eve.currentTarget);
+function removeOne(event) {
+    const thisChore = event.target.parentNode;
+    thisChore.style.display = 'none';
+    // event.target.parentNode.classList.toggle("hide");
+    // thisChore.parentNode.removeChild(thisChore);
 }
 
 /*
@@ -125,12 +136,13 @@ function showRemoveBtn() {
 /*
 -------------------------------------------------
 (a) Add the checkbox statuses at page reloads and remove alls.
-(b) Remove single items
+(b) On reload, the state of the removed chores gets reset.
 (e) Reordering the list items
 (f) Edit the items
 (h) Filter
 (i) Sort
 (j) The checked one should go down
+(b) Done - Remove single items
 (c) Done - RWD
 (d) Done - Styling
 (g) Done - Bug: The topmost item that's ticked loses its state with page reload
